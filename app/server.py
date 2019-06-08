e2ecfaa110bc17a9b1eab837b36faa6f1ddb1206
harshit patel <hp12180@yahoo.com>
2019-06-08 21:41:16 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,9 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 
+               'Lung Lesion', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 'Pneumothorax', 
+               'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices']
 path = Path(__file__).parent
 
 app = Starlette()
