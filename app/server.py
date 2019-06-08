b3a3162d2400d597f5a6ad8435a67696ccc4daf0
harshit patel <hp12180@yahoo.com>
2019-06-08 22:55:23 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=17yTRKBu8Sy3UCK6AeSl4QkniZgSgULpp'
+export_file_name = 'm-epoch1-14042019-132417.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Lung Lesion', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices']
 path = Path(__file__).parent
 
 app = Starlette()
